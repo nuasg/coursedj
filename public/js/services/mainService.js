222a2748578e7af08a9d82a1347cbd2e21e40464
Michael Wang <mwang@splunk.com>
2015-08-17 05:03:23 -0700

    Use debug key

--- public/js/services/mainService.js
+++ public/js/services/mainService.js
@@ -4,9 +4,9 @@ var app = angular.module('mainService', []);
 
 app.factory('ASG', function($http){
 
-	var NOT_A_KEY = "a3iSOsJ77pgC8BnX"; //LIVE
-	//var NOT_A_KEY = "P2VVnfB0PwGQpdqh"; //DEBUG
-	return{
+	// var NOT_A_KEY = "a3iSOsJ77pgC8BnX"; // LIVE
+	var NOT_A_KEY = "P2VVnfB0PwGQpdqh"; // DEBUG
+	return {
 		getSubjects: function(term){
 			return $http.get('http://api.asg.northwestern.edu/subjects/?key=' + NOT_A_KEY+ '&term=' + term);
 		},
