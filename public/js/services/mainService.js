13bfb8814c8c8c432f4477223fc0172fad6b1020
Michael Wang <mwang@splunk.com>
2015-09-16 18:03:40 -0700

    Restore the live key

--- public/js/services/mainService.js
+++ public/js/services/mainService.js
@@ -4,8 +4,8 @@ var app = angular.module('mainService', []);
 
 app.factory('ASG', function($http){
 
-	// var NOT_A_KEY = "a3iSOsJ77pgC8BnX"; //LIVE
-	var NOT_A_KEY = "P2VVnfB0PwGQpdqh"; //DEBUG
+	var NOT_A_KEY = "a3iSOsJ77pgC8BnX"; //LIVE
+	// var NOT_A_KEY = "P2VVnfB0PwGQpdqh"; //DEBUG
 	return{
 		getSubjects: function(term){
 			return $http.get('http://api.asg.northwestern.edu/subjects/?key=' + NOT_A_KEY+ '&term=' + term);
